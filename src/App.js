7fdf9518fbe2f8561e764ce6653f31f9e92e672d
mktintumon <mktintumon@gmail.com>
2022-05-04 00:03:39 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import MovieList from './components/MovieList';
+import Favourites from './components/Favourites';
 
 function App() {
   return (
     <>
     <Navbar />
-    <Banner/>
-    <MovieList/>
+    {/* <Banner/>
+    <MovieList/> */}
+    <Favourites/>
     </>
   );
 }
